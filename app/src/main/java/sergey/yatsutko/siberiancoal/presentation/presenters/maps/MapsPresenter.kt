282313537427979237651c15e2d5a9fa23512f65
Mirocidij <Mirocidij@users.noreply.github.com>
2019-04-15 14:34:03 +0700

    MapsActivity -> MVP

--- app/src/main/java/sergey/yatsutko/siberiancoal/presentation/presenters/maps/MapsPresenter.kt
+++ app/src/main/java/sergey/yatsutko/siberiancoal/presentation/presenters/maps/MapsPresenter.kt
@@ -1,4 +1,28 @@
 package sergey.yatsutko.siberiancoal.presentation.presenters.maps
 
-class MapsPresenter {
+import com.arellomobile.mvp.InjectViewState
+import com.arellomobile.mvp.MvpPresenter
+import com.google.android.gms.maps.model.LatLng
+import sergey.yatsutko.siberiancoal.R
+
+
+@InjectViewState
+class MapsPresenter: MvpPresenter<MapsView>() {
+
+    private lateinit var point: LatLng
+
+    fun pointWasChecked(point: LatLng) {
+        this.point = point
+        viewState.refreshFlag(this.point)
+    }
+
+    fun acceptButtonWasPressed() {
+        if (point.latitude == 0.0 || point.longitude == 0.0) {
+            viewState.showError(R.string.uncheckEndPoint)
+            return
+        }
+
+        viewState.saveCoordinatesAndFinish(point)
+    }
+
 }
--- app/src/main/java/sergey/yatsutko/siberiancoal/presentation/presenters/maps/MapsView.kt
+++ app/src/main/java/sergey/yatsutko/siberiancoal/presentation/presenters/maps/MapsView.kt
@@ -1,4 +1,12 @@
 package sergey.yatsutko.siberiancoal.presentation.presenters.maps
 
-interface MapsView {
+import com.arellomobile.mvp.MvpView
+import com.google.android.gms.maps.model.LatLng
+
+interface MapsView : MvpView {
+
+    fun refreshFlag(point: LatLng)
+    fun saveCoordinatesAndFinish(point: LatLng)
+    fun showError(messageRes: Int)
+
 }
