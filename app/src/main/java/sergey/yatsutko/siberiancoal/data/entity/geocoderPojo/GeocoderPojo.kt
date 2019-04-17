9959381678bdfbcd478f006cd8f5b8005518a6aa
Mirocidij <Mirocidij@users.noreply.github.com>
2019-04-17 15:42:43 +0700

    GeocoderPojo fix

--- app/src/main/java/sergey/yatsutko/siberiancoal/data/entity/geocoderPojo/GeocoderPojo.kt
+++ app/src/main/java/sergey/yatsutko/siberiancoal/data/entity/geocoderPojo/GeocoderPojo.kt
@@ -9,23 +9,4 @@ class GeocoderPojo {
     @Expose
     lateinit var response: Response
 
-    val kind = response.geoObjectCollection
-        .featureMember[0]
-        .geoObject
-        .metaDataProperty
-        .geocoderMetaData
-        .kind
-
-    val point = response.geoObjectCollection
-        .featureMember[0]
-        .geoObject
-        .point
-        .pos
-
-    val address = response.geoObjectCollection
-        .featureMember[0]
-        .geoObject
-        .metaDataProperty
-        .geocoderMetaData
-        .text
 }
